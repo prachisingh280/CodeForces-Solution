48b93bd43cb17a1a9959630a98178ff19146b6a9
Prachi Singh <prachisingh.6824@gmail.com>
2026-08-03 14:35:09 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+       int n;
+       cin>>n;
+       int arr[n];
+       for(int i=1; i<=n; i++)
+       {
+           arr[i] = i;
+       }
+       
+       for(int i=n; i>=1; i--)
+       {
+           cout<<arr[i]<<" ";
+       }
+       
+       
+       cout<<"
+";
+       }
+        
+        
+    }
+ 
+ 
