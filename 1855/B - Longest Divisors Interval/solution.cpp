8cb75dfff0d1f493e91bb0f7238c41a1b7a89524
Prachi Singh <prachisingh.6824@gmail.com>
2026-08-03 14:41:19 +0530

    Add Longest Divisors Interval [B] from Codeforces

---
+++ 1855/B - Longest Divisors Interval/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        
+        long long i = 1;
+        
+        while(n%i==0)
+        {
+            i++;
+        }
+        
+        cout<<i-1<<"
+";
+    }
+}
