4afac6c342c98b3921f3c270a85f96bcf00286f4
Prachi Singh <prachisingh.6824@gmail.com>
2026-08-03 14:35:33 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+ 
+ 
+        if ((n + 1) % 3 == 0 || (n - 1) % 3 == 0)
+        {
+            cout << "First";
+            
+        }
+        else
+        {
+            cout << "Second";
+            
+        }
+ 
+        cout<<"
+";
+ 
+    }
+ 
+}
