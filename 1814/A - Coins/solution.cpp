70c77d295b2fe04bafe7c940e121263c12c1c901
Prachi Singh <prachisingh.6824@gmail.com>
2026-08-03 14:37:22 +0530

    Add Coins [A] from Codeforces

---
+++ 1814/A - Coins/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n,k;
+        cin >> n>>k;
+        
+        if(n%2==0) 
+        {
+            cout<<"yes";
+        }
+        else
+        {
+            if(k%2==0) cout<<"no";
+            else cout<<"yes";
+        }
+        
+        cout<<"
+";
+      
+    }
+ 
+}
