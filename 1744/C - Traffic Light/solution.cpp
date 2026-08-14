ead86a4db0ce031ea9eb07a1e316a5fed4f4e604
Prachi Singh <prachisingh.6824@gmail.com>
2026-08-15 00:18:39 +0530

    Add Traffic Light [C] from Codeforces

--- 1744/C - Traffic Light/solution.cpp
+++ 1744/C - Traffic Light/solution.cpp
@@ -1,49 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-int main()
+int main() 
 {
-	long long t;
-	cin >> t; // Read the number of test cases
-	while (t--)
-	{
-		long long n;
-		cin >> n; // Read the length of the string
-		char color;
-		cin >> color; // Read the current color of the traffic light
-		string s;
-		cin >> s; // Read the traffic light color sequence
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int n;
+    cin>>n;
+    char c;
+    cin>>c;
+    string s;
+    cin>>s;
+    vector<int>ans;
+    vector<int>g;
+    
+    for(int i=0; i<n; i++)
+    {
+        if(s[i] == 'g')
+        {
+            g.push_back(i+1);
+        }
+    }
  
-		// Duplicate the string to simulate the cyclic nature of the traffic light
-		s += s;
- 
-		// Update n to reflect the new length of the duplicated string
-		n *= 2;
- 
-		// Initialize variables to track the last seen green light index and the maximum wait time
-		long long last_green_index = -1;
-		long long max_seconds = INT_MIN;
- 
-		// Traverse the string from the end to the beginning
-		for (int i = n - 1; i >= 0; i--)
-		{
-			// Update the last seen green light index
-			if (s[i] == 'g')
-				last_green_index = i;
- 
-			// If the current color matches the given color, calculate the wait time
-			if (s[i] == color)
-			{
-				long long difference = last_green_index - i;
-				// Update the maximum wait time
-				max_seconds = max(max_seconds, difference);
-			}
-		}
-		// Output the maximum wait time for the current test case
-		cout << max_seconds << endl;
-	}
-	return 0;
+    for(int i=0; i<n; i++)
+    {
+        if(s[i]==c)
+        {
+            for(int j=0; j<g.size(); j++)
+            {
+                if(g[j]>=i+1)
+                {
+                    ans.push_back(g[j]-(i+1));
+                    break;
+                }
+                else if((i+1)>(g[g.size()-1]))
+                {
+                    ans.push_back(n-(i+1)+g[0]);
+                    break;
+                }
+            }
+        }
+    }
+    
+    int ansss = *max_element(ans.begin(),ans.end());
+    cout<<ansss<<"
+";
+}
 }
  
-// Time Complexity (TC): O(n) = O(2*10^5)
-// Space Complexity (SC): O(n) = O(2*10^5)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
